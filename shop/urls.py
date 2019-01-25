0e12896d5bef500a4cd56ee87f5a9dc3d4467def
Dan <dcollins31@gmail.com>
2019-01-25 15:41:45 -0600

    Worked on c_slug and c_page

--- shop/urls.py
+++ shop/urls.py
@@ -8,6 +8,5 @@ app_name = 'shop'
 # 🗺 Map urls
 urlpatterns = [
     path('', views.allProdCat, name='allProdCat'),
-    # 1st 1 maps all products w/o category|2nd 1 maps all products by category
-    path('<slug:c_slug>', views.allProdCat, name='products_by_category'),
+    path('<slug:c_slug>/', views.allProdCat, name='products_by_category'),
 ]
